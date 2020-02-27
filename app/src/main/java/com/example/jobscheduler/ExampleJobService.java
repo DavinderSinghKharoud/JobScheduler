05bf13ea4c85230e7bfc34bad9c93b078fbb6b55
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-02-26 22:56:20 -0500

    Implementing Job Service

---
+++ app/src/main/java/com/example/jobscheduler/ExampleJobService.java
@@ -0,0 +1,56 @@
+package com.example.jobscheduler;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.nfc.Tag;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class ExampleJobService extends JobService {
+
+    private static final String TAG = "ExampleJobService";
+    private boolean jobCancelled = false;
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        doBackground(jobParameters);
+
+        return false;
+    }
+
+    private void doBackground(final JobParameters jobParameters) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for( int i = 0; i<10; i++ ){
+                    Log.d(TAG, "run: " + i);
+
+                    if( jobCancelled ){
+                        return;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.d(TAG, "Job finished");
+                jobFinished(jobParameters, false);
+            }
+
+
+        }).start();
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        Log.d(TAG,"Job cancelled before completion");
+
+        jobCancelled = false;
+        return true;
+    }
+}
--- app/src/main/java/com/example/jobscheduler/MainActivity.java
+++ app/src/main/java/com/example/jobscheduler/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jobscheduler;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.content.ComponentName;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     public void scheduleJob(View view) {
+
+        ComponentName componentName = new ComponentName(this, ExampleJobService.class);
+
+        JobInfo jobInfo = new JobInfo.Builder( 123, componentName )
+                .setRequiresCharging(true)
+                .setRequiredNetworkType( JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 1000 * 60)
+                .build();
+
         
+
     }
 
     public void cancelJob(View view) {
